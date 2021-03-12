dda07f8aca13c95d981470ddd0e453197c44a5aa
Joaquin Moreno <jmavs21@gmail.com>
2021-03-11 19:03:13 -0600

    Fix remove of styles.

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ javascript: (() => {
   const [repoUrl, userAndRepo, branch] = getRepoInfo();
 
   const cssStyle = document.createElement('style');
+  cssStyle.setAttribute('id', 'css-style-pr');
   cssStyle.innerHTML = `#editor-pr { padding-bottom: 35%; } #editor-pr, #repo-pr { width: fit-content; font-family: sans-serif; font-size: small; } .folder-container-pr, .file-pr { display: block; padding: 2px 2px 1px 10px; } .folder-pr { color: black; } .file-pr { color: black; } .folder-pr, .file-pr { cursor: pointer; } .folder-pr:hover, .file-pr:hover { background: #00ffff; } .folder-pr:before, .file-pr:before { padding-right: 5px; } a { text-decoration: none; } .i-folder-pr:before { content: '\\1F4C1'; } .i-folder-pr-o:before { content: '\\1F4C2'; } .i-file-pr-code-o:before { content: '\\1F4C4'; } .modal-pr { position: fixed; z-index: 1; padding-top: 100px; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgb(0, 0, 0); background-color: rgba(0, 0, 0, 0.4); } .modal-pr-content { background-color: #fefefe; margin: auto; padding: 10px; border: 1px solid #888; width: fit-content; } .close-pr { color: #aaaaaa; float: right; font-size: 20px; } .close-pr:hover, .close-pr:focus { color: #000; text-decoration: none; cursor: pointer; }`;
   document.head.appendChild(cssStyle);
 
@@ -97,13 +98,17 @@ javascript: (() => {
     elem !== event.currentTarget &&
     elem.classList.contains('folder-pr');
 
-  const modal = document.getElementById('modal-pr');
+  const removeModalAndStyle = () => {
+    modalDiv.remove();
+    cssStyle.remove();
+  };
+
   const close = document.getElementsByClassName('close-pr')[0];
   close.onclick = () => {
-    modal?.remove();
+    removeModalAndStyle();
   };
   window.onclick = (event) => {
-    if (event.target === modal) modal?.remove();
+    if (event.target === modalDiv) removeModalAndStyle();
   };
 
   const raw = {
